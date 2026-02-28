500a79719928e276b3473b146734f17604c1e93a
Adriano Zambrana Marchetti <adrianozambrana@protonmail.com>
2026-02-28 11:02:14 -0400

    Plantilla inicial

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { APITester } from "./APITester";
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <p>
+      Hello! App is working!
+    </p>
+  );
+}
+
+export default App;
